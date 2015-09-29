52074cef9a0a264f5999092d2c66e06873c577aa
thanhld94 <thanhld94@gmail.com>
2015-09-29 00:32:11 -0500

    Add pacdot generator

---
+++ Pacman/Assets/Scripts/DotGeneratorBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DotGeneratorBehavior : MonoBehaviour {
+
+	public GameObject DotObject;
+	public LayerMask WhatIsWall;
+	private float mRadius = 0.05f;
+
+	void Start () {
+		for ( int i = 2; i < 28; i++ ) 
+			for ( int j = 2; j < 34; j++ ) {
+				Vector2 pos = new Vector2(i,j);
+				if ( isValidPosition(pos) ) 	
+					Instantiate( DotObject, pos, Quaternion.identity );
+			}
+					
+	}
+
+	private bool isValidPosition( Vector2 pos ) {
+		DotObject.GetComponent <BoxCollider2D>().enabled = false;
+		bool isValid = (Physics2D.OverlapCircle(pos, mRadius, WhatIsWall) == null);
+		DotObject.GetComponent <BoxCollider2D>().enabled = true;
+		return isValid;
+	}
+
+	void Update () {
+		
+	}
+}
--- Pacman/Assets/Scripts/PacDotBehavior.cs
+++ Pacman/Assets/Scripts/PacDotBehavior.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class PacDotBehavior : MonoBehaviour {
 
+	static private int mScore = 0;
+
 	void OnTriggerEnter2D (Collider2D other) {
-		if (other.gameObject.tag == "Player")
+		if (other.gameObject.tag == "Player") {
+			mScore++;
 			Destroy(gameObject);
+		}
+	}
+
+	public int GetScore() {
+		return mScore;
 	}
 }
